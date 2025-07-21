a3af0ebe0fce4cca62d6251c0081d882dbda7e6b
SA <54625055+sa713@users.noreply.github.com>
2025-07-21 19:37:58 +0300

    v1.0

--- bot.py
+++ bot.py
@@ -2,14 +2,15 @@ import asyncio
 from aiogram import Bot, Dispatcher, F
 from handlers import router
 from config import BOT_TOKEN
+from scheduler import setup_scheduler
 
 async def main():
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
     dp.include_router(router)
 
-    await setup_scheduler(bot) 
-    
+    await setup_scheduler(bot)
+
     try:
         print("🚀 Бот запускается...")
         await dp.start_polling(bot)
